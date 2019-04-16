7f550a3e6ae632d7c45171ae85b8a15bb7c79a46
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-16 03:42:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,6 @@
 class Student 
   
-  attr_accessor :first_name, :last_name
-  
-  def initialize
-    @knowledge = []
-  end 
-  
-  def learn(string_K)
-    @knowledge << string_K
-  end
-  
-  def knowledge
-    @knowledge
-  end 
+
   
 
 end 
